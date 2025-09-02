6a2c643efd18b0bc6a7e3095cfe1d6a0d0fbcfe8
Borja Marquez de la Plata <bmarquezdelaplata@uc.cl>
2025-09-01 23:39:06 -0400

    feat: first dashboard prototype

--- tasks/dashboard_designer.py
+++ tasks/dashboard_designer.py
@@ -1,7 +1,15 @@
 import psycopg2
 from psycopg2.extras import RealDictCursor
+from pydantic_ai import Agent
+from google.genai import Client
+from pydantic_ai.models.google import GoogleModel
+from pydantic_ai.providers.google import GoogleProvider
 import os
 from dotenv import load_dotenv
+import logfire
+
+logfire.configure()  
+logfire.instrument_pydantic_ai()
 
 load_dotenv()
 DB_HOST = os.getenv('DB_HOST')
@@ -9,6 +17,7 @@ DB_PORT = os.getenv('DB_PORT')
 DB_NAME = os.getenv('DB_NAME')
 DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
+USER_ID = os.getenv('USER_ID')
 
 # Replace with your Supabase credentials
 conn = psycopg2.connect(
@@ -44,12 +53,72 @@ def transform_to_tables_schema(rows):
     return tables_schema
 
 # Run raw SQL
-cur.execute(SQL_get_tables_with_schema)
-rows = cur.fetchall()
+# cur.execute(SQL_get_tables_with_schema)
+# rows = cur.fetchall()
 
-tables_schema = transform_to_tables_schema(rows)
-print(tables_schema)
+# tables_schema = transform_to_tables_schema(rows)
+# print(tables_schema)
 
+# ------------ MODEL ----------------------
+
+GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
+model_options = ['gemini-2.5-flash', 'gemini-2.5-flash-lite', 'gemma-3-27b-it']
+
+client = Client(
+    api_key=GOOGLE_API_KEY,
+)
+provider = GoogleProvider(client=client)
+model = GoogleModel(model_options[0], provider=provider)
+
+INSTRUCTIONS = """
+You are an expert in HTML, CSS, and JavaScript. You will build small front visualizations based on requests from the user.
+The user will include its request in the <user> tag. Design data, such as viewport size, will be included in the <design> tag if provided.
+You must retrieve the data from the userbase to create the visualizations.
+You will output code inside <output> tags.
+"""
+
+# INSTRUCTIONS = """
+# You are an expert in database management. You will help the user with his requests
+# """
+
+designer = Agent(
+    model,
+    instructions=INSTRUCTIONS
+)
+
+@designer.instructions
+def get_user_id() -> str:
+    return f"The user id is: {USER_ID}"
+
+@designer.tool_plain
+def get_database_schema() -> dict:
+    """Returns the Schema of the database"""
+    cur.execute(SQL_get_tables_with_schema)
+    rows = cur.fetchall()
+    tables_schema = transform_to_tables_schema(rows)
+    return tables_schema
+
+@designer.tool_plain
+def run_sql_query(query: str) -> dict:
+    """Runs a SQL query and returns the result. If there is an error it returns the error message so you can try again"""
+    try:    
+        cur.execute(query)
+        rows = cur.fetchall()
+        return rows
+    except Exception as e:
+        return str(e)
+
+
+result = designer.run_sync(
+    "Make a pie chart of the activities I have done between 25/08/2025 - 01/09/2025"
+)
+
+with open("outputs/dashboard_designer.html", "w") as f:
+    f.write(result.output)
+
+print(result.output)
 
 cur.close()
 conn.close()
+
+
