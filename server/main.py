9b082e9b3df5b3c70af09e461e0dc02842dd7a94
Borja Marquez de la Plata <bmarquezdelaplata@uc.cl>
2025-12-11 12:11:02 -0300

    feat: created an agent tasks class to store all tasks

--- server/main.py
+++ server/main.py
@@ -2,12 +2,18 @@ import grpc.aio
 import asyncio
 
 import ai_service_pb2, ai_service_pb2_grpc
-from ai_tasks.q_and_a import q_a_with_model
+from ai_core.agent_tasks import AgentTasks
 
 class AIServiceServicer(ai_service_pb2_grpc.AIServiceServicer):
+    def __init__(self):
+        self.agent_tasks = AgentTasks()
+
     async def QA(self, request, context):
-        async for text in q_a_with_model(request.question):
+        async for text in self.agent_tasks.q_a_with_model(request.question, request.model):
             yield ai_service_pb2.Answer(answer=text)
+
+    async def GetAvailableModels(self, request, context):
+        return ai_service_pb2.ReturnAvailableModels(models=self.agent_tasks.get_available_models())
 
 async def serve() -> None:
     server = grpc.aio.server()
